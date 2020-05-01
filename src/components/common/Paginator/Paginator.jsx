eca7494241fff4cdb6b36646ef63c70fceffe8e9
AFcodCloud <alfibrz@gmail.com>
2020-05-02 05:45:15 +0800

    Paginator tests and new paginator in musicComponent lesson-93

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -2,10 +2,11 @@
  import s from "./Paginator.module.css"
 
 const Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize=10})=>{
+    //страницы
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {pages.push(i)}
-    
+    //порции
     let portionCount = Math.ceil(pagesCount/portionSize);
     let [portionNumber, setPortionNumber]=useState(1);
     let leftPortionNumber = (portionNumber-1)*portionSize+1;
